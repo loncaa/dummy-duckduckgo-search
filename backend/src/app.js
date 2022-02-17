2f0b8835afef385b43717931743e9407b63cf772
--local <antonio@dotmatrics.com>
2022-02-17 23:15:07 +0100

    use helmet

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
 
 var {exposeApplicationRoutes} = require('./controllers/routes')
 
 var app = express();
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
